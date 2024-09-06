42a09caddc5c38b33240373a57146ab07b50baf6
Hikam-Baidawih <hikambaidawih@gmail.com>
2024-09-06 16:04:53 +0700

    first commit

---
+++ src/belajar/Main.java
@@ -0,0 +1,23 @@
+
+package belajar;
+import java.util.Scanner;
+/**
+ *
+ * @author hikam
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        int hours = sc.nextInt();
+        float paid = sc.nextFloat();
+        float hasil = (float) hours * paid;
+        String salary = String.format("%.2f", hasil);
+        System.out.println("NUMBER = " + number);
+        System.out.println("SALARY = U$ " + salary);
+    }
+    
+}
