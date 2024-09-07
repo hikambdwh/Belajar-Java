1961281ba236a7695b93f0925748d2661ee45112
hikambdwh <hikambaidawih@gmail.com>
2024-09-07 15:45:58 +0700

    memodifikasi operasi ternary

--- src/belajar/ternary.java
+++ src/belajar/ternary.java
@@ -3,29 +3,41 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package belajar;
+
 import java.util.Scanner;
+
 /**
  *
  * @author hikap
  */
 public class ternary {
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        
+
         int nilai;
         int y;
         String x;
-        
+
         System.out.print("Masukan nilai: ");
         nilai = sc.nextInt();
-        
-        // Variabel x = (ekspresi) ? (true) : (false)
-        y = (nilai > 10) ? (nilai * 100) : (nilai - 10);
-        x = (nilai > 10) ? ("kamu benar") : ("Kamu salah");
-        
-        System.out.println(y);
-        System.out.println(x);
-    
+        if (nilai <= 10) {
+            while (nilai <= 15) {
+                
+                // Variabel x = (ekspresi) ? (true) : (false)
+                y = (nilai > 10) ? (nilai * 100) : (nilai - 10);
+                x = (nilai > 10) ? ("kamu benar") : ("Kamu salah");
+
+                System.out.println(y);
+                System.out.println(x);
+                nilai++;
+
+            }
+        } else {
+            System.out.println("NILAI SALAH");
+
+        }
+
     }
 }
