a94f5080ef8f3a7b1294325ab174eed46ca1f19f
hikambdwh <hikambaidawih@gmail.com>
2024-09-11 01:17:20 +0700

    Membuat file BelajarArrays.java

---
+++ src/tutorial/BelajarArrays.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tutorial;
+import java.util.*;
+/**
+ *
+ * @author hikap
+ */
+public class BelajarArrays {
+    public static void main(String[] args){
+    
+        int[] angka = {12,13,22,21,19,42,31,10};
+        System.out.println(angka.length);
+        
+        System.out.println(Arrays.toString(angka));
+        
+        for(int nomor : angka){
+            System.out.println(nomor);
+        }
+    }
+}
